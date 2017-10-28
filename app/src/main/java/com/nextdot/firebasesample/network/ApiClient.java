83d9c66335b5261cb7a5765abac4f2981c0c5d6c
abir <imtiazkalamabir@gmail.com>
2017-10-28 19:34:41 +0600

    Modified ApiClient && AndroidManifest

--- app/src/main/java/com/nextdot/firebasesample/network/ApiClient.java
+++ app/src/main/java/com/nextdot/firebasesample/network/ApiClient.java
@@ -1,8 +1,27 @@
 package com.nextdot.firebasesample.network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by abir on 28/10/2017.
  */
 
 public class ApiClient {
+
+    public static final String BASE_URL = "https://someurl.com/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getApiClient(){
+
+        if (retrofit == null) {
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
 }
